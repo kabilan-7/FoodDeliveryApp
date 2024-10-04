207ff88c73a2b962fec57a72311a7d87eb2172b9
kabilan-7 <murugankabilan39@gmail.com>
2024-10-04 21:18:46 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ connectDB()
  
 //api end points
 app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
+app.use("/images",express.static(path.resolve('uploads')))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
 app.use("/api/order",orderRouter)
